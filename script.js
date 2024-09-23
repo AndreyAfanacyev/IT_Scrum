1ddeb70181fe1ddc62d37b4d18f63f9c2e587939
AndreyAfanacyev <60614165+AndreyAfanacyev@users.noreply.github.com>
2024-09-23 18:03:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+slides[currentSlide].classList.add('active');
+
+document.getElementById('next').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
